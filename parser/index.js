9b3de714b600038132c2222b4aec0415e90afad0
Stanislav Lesiuk <stanislav.lesiuk@intive.com>
2018-04-06 11:13:49 +0200

    Add basic node parser

---
+++ parser/index.js
@@ -0,0 +1,49 @@
+const request = require('request');
+const cheerio = require('cheerio');
+const tress = require('tress');
+
+const URL = 'https://www.otomoto.pl/osobowe/volkswagen/polo/iv-2001-2009/?search%5Bfilter_float_price%3Afrom%5D=9000&search%5Bfilter_float_price%3Ato%5D=16000';
+let results = [];
+const selectors = {
+  title: '.offer-title',
+  price: '.offer-price__number',
+  year: '.offer-item__params-item:nth-child(1)',
+  header: '.offer-header',
+  detailPageLink: '.offer-title__link'
+};
+
+const queue = tress(function (url, callback) {
+  request.get(url, function (err, res, body) {
+    if (err) {
+      throw err;
+    }
+    const $ = cheerio.load(body);
+
+    //check if detail page
+    if ($(selectors.header).length) {
+      results.push({
+        title: $(selectors.title).text().trim(),
+        price: $(selectors.price).text().trim(),
+        year: $(selectors.year).text().trim(),
+        href: url
+      });
+    }
+
+    //here we push link to every detail page
+    $(selectors.detailPageLink).each(function () {
+      const href = $(this).attr('href');
+      console.log('pushing link to detail page: ', href);
+      queue.push(href);
+    });
+
+    callback(); //callback = queue.drain at the end
+  });
+});
+
+// will be executed when links end
+queue.drain = function () {
+  console.log(results);
+};
+
+// add first URL to queue
+queue.push(URL);
