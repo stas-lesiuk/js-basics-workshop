f83f359f9790009b18b571dd2b2fe07debd61d64
Stanislav Lesiuk <stanislav.lesiuk@intive.com>
2018-05-15 13:39:15 +0200

    Added flex styles for news, added active link styles, change Link to NavLink

--- react-app/app/scripts/containers/Navbar.jsx
+++ react-app/app/scripts/containers/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import './navbar.scss';
 
 const Navbar = () => (
   <nav className="menu">
-    <Link to="/home">Home</Link>
-    <Link to="/news">News</Link>
+    <NavLink to="/home" activeClassName="active">Home</NavLink>
+    <NavLink to="/news" activeClassName="active">News</NavLink>
   </nav>
 );
 
